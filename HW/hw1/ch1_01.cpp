9c67c1d750b96f74f2f846a410c10b9258ad7902
yungder <alum438217@gmail.com>
2026-09-06 20:10:55 +0800

    新增 ch1_01.cpp 檔案，實作簡單的加法運算程式

---
+++ HW/hw1/ch1_01.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main() {
+	int firstNumber;
+	int secondNumber;
+
+	std::cin >> firstNumber >> secondNumber;
+	std::cout << firstNumber << " + " << secondNumber << " = "
+			  << firstNumber + secondNumber << '\n';
+
+	return 0;
+}
