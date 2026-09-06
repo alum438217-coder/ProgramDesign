3ca65d7e924957296e44cf0ca895666ab167918d
yungder <alum438217@gmail.com>
2026-09-06 16:58:57 +0800

    新增 C++ 檔案及建置任務，包含主程式及數學結構的實作

--- main.cpp
+++ main.cpp
@@ -1,6 +1,16 @@
 #include <iostream>
+#include <cstdlib>
 using namespace std;
+
+struct Math {
+    int abs(int value) {
+        return std::abs(value);
+    }
+};
+
 int main(void){
+    Math math;
     cout << "Hello, world." << endl;
+    cout << math.abs(-5) << endl;
     return 0;
 }
